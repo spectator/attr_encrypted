15765d9f353b80d6876115153b811abece659fa2
Yury Velikanau <yury.velikanau@gmail.com>
2011-02-20 21:53:40 +0200

    Remove whitespaces.

--- lib/attr_encrypted.rb
+++ lib/attr_encrypted.rb
@@ -6,20 +6,20 @@ module AttrEncrypted
     base.attr_encrypted_options = {}
     base.instance_variable_set('@encrypted_attributes', {})
   end
-  
+
   # Default options to use with calls to <tt>attr_encrypted</tt>.
   #
   # It will inherit existing options from its parent class
   def attr_encrypted_options
     @attr_encrypted_options ||= superclass.attr_encrypted_options.nil? ? {} : superclass.attr_encrypted_options.dup
   end
-  
+
   # Sets default options to use with calls to <tt>attr_encrypted</tt>.
   def attr_encrypted_options=(options)
     @attr_encrypted_options = options
   end
-  
-  # Contains a hash of encrypted attributes with virtual attribute names as keys and real attribute 
+
+  # Contains a hash of encrypted attributes with virtual attribute names as keys and real attribute
   # names as values
   #
   # Example
@@ -32,7 +32,7 @@ module AttrEncrypted
   def encrypted_attributes
     @encrypted_attributes ||= superclass.encrypted_attributes.nil? ? {} : superclass.encrypted_attributes.dup
   end
-  
+
   # Checks if an attribute has been configured to be encrypted
   #
   # Example
@@ -47,42 +47,42 @@ module AttrEncrypted
   def attr_encrypted?(attribute)
     encrypted_attributes.keys.include?(attribute.to_s)
   end
-  
+
   protected
-  
+
     # Generates attr_accessors that encrypt and decrypt attributes transparently
     #
     # Options (any other options you specify are passed to the encryptor's encrypt and decrypt methods)
     #
-    #   :attribute      => The name of the referenced encrypted attribute. For example 
-    #                      <tt>attr_accessor :email, :attribute => :ee</tt> would generate an 
+    #   :attribute      => The name of the referenced encrypted attribute. For example
+    #                      <tt>attr_accessor :email, :attribute => :ee</tt> would generate an
     #                      attribute named 'ee' to store the encrypted email. This is useful when defining
     #                      one attribute to encrypt at a time or when the :prefix and :suffix options
     #                      aren't enough. Defaults to nil.
     #
     #   :prefix         => A prefix used to generate the name of the referenced encrypted attributes.
-    #                      For example <tt>attr_accessor :email, :password, :prefix => 'crypted_'</tt> would 
-    #                      generate attributes named 'crypted_email' and 'crypted_password' to store the 
+    #                      For example <tt>attr_accessor :email, :password, :prefix => 'crypted_'</tt> would
+    #                      generate attributes named 'crypted_email' and 'crypted_password' to store the
     #                      encrypted email and password. Defaults to 'encrypted_'.
     #
     #   :suffix         => A suffix used to generate the name of the referenced encrypted attributes.
-    #                      For example <tt>attr_accessor :email, :password, :prefix => '', :suffix => '_encrypted'</tt>  
-    #                      would generate attributes named 'email_encrypted' and 'password_encrypted' to store the 
+    #                      For example <tt>attr_accessor :email, :password, :prefix => '', :suffix => '_encrypted'</tt>
+    #                      would generate attributes named 'email_encrypted' and 'password_encrypted' to store the
     #                      encrypted email. Defaults to ''.
     #
-    #   :key            => The encryption key. This option may not be required if you're using a custom encryptor. If you pass 
-    #                      a symbol representing an instance method then the :key option will be replaced with the result of the 
-    #                      method before being passed to the encryptor. Objects that respond to :call are evaluated as well (including procs). 
+    #   :key            => The encryption key. This option may not be required if you're using a custom encryptor. If you pass
+    #                      a symbol representing an instance method then the :key option will be replaced with the result of the
+    #                      method before being passed to the encryptor. Objects that respond to :call are evaluated as well (including procs).
     #                      Any other key types will be passed directly to the encryptor.
     #
     #   :encode         => If set to true, attributes will be encoded as well as encrypted. This is useful if you're
-    #                      planning on storing the encrypted attributes in a database. The default encoding is 'm*' (base64), 
+    #                      planning on storing the encrypted attributes in a database. The default encoding is 'm*' (base64),
     #                      however this can be overwritten by setting the :encode option to some other encoding string instead of
-    #                      just 'true'. See http://www.ruby-doc.org/core/classes/Array.html#M002245 for more encoding directives. 
+    #                      just 'true'. See http://www.ruby-doc.org/core/classes/Array.html#M002245 for more encoding directives.
     #                      Defaults to false unless you're using it with ActiveRecord or DataMapper.
     #
     #   :marshal        => If set to true, attributes will be marshaled as well as encrypted. This is useful if you're planning
-    #                      on encrypting something other than a string. Defaults to false unless you're using it with ActiveRecord 
+    #                      on encrypting something other than a string. Defaults to false unless you're using it with ActiveRecord
     #                      or DataMapper.
     #
     #   :encryptor      => The object to use for encrypting. Defaults to Encryptor.
@@ -91,12 +91,12 @@ module AttrEncrypted
     #
     #   :decrypt_method => The decrypt method name to call on the <tt>:encryptor</tt> object. Defaults to :decrypt.
     #
-    #   :if             => Attributes are only encrypted if this option evaluates to true. If you pass a symbol representing an instance 
-    #                      method then the result of the method will be evaluated. Any objects that respond to :call are evaluated as well. 
+    #   :if             => Attributes are only encrypted if this option evaluates to true. If you pass a symbol representing an instance
+    #                      method then the result of the method will be evaluated. Any objects that respond to :call are evaluated as well.
     #                      Defaults to true.
     #
-    #   :unless         => Attributes are only encrypted if this option evaluates to false. If you pass a symbol representing an instance 
-    #                      method then the result of the method will be evaluated. Any objects that respond to :call are evaluated as well. 
+    #   :unless         => Attributes are only encrypted if this option evaluates to false. If you pass a symbol representing an instance
+    #                      method then the result of the method will be evaluated. Any objects that respond to :call are evaluated as well.
     #                      Defaults to false.
     #
     # You can specify your own default options
@@ -125,29 +125,29 @@ module AttrEncrypted
     #
     #   See README for more examples
     def attr_encrypted(*attrs)
-      options = { 
-        :prefix => 'encrypted_', 
-        :suffix => '', 
-        :encryptor => Encryptor, 
+      options = {
+        :prefix => 'encrypted_',
+        :suffix => '',
+        :encryptor => Encryptor,
         :encrypt_method => :encrypt,
         :decrypt_method => :decrypt,
-        :encode => false, 
-        :marshal => false, 
-        :if => true, 
-        :unless => false 
+        :encode => false,
+        :marshal => false,
+        :if => true,
+        :unless => false
       }.merge(attr_encrypted_options).merge(attrs.last.is_a?(Hash) ? attrs.pop : {})
       options[:encode] = 'm' if options[:encode] == true
-      
+
       attrs.each do |attribute|
         encrypted_attribute_name = options[:attribute].nil? ? options[:prefix].to_s + attribute.to_s + options[:suffix].to_s : options[:attribute].to_s
-        
+
         encrypted_attributes[attribute.to_s] = encrypted_attribute_name
-        
+
         # TODO: IMO there is too many #to_sym calls all over the code, this should be refactored.
         instance_methods_as_symbols = RUBY_VERSION < '1.9' ? instance_methods.map(&:to_sym) : instance_methods
         attr_reader encrypted_attribute_name.to_sym unless instance_methods_as_symbols.include?(encrypted_attribute_name.to_sym)
         attr_writer encrypted_attribute_name.to_sym unless instance_methods_as_symbols.include?("#{encrypted_attribute_name}=".to_sym)
-        
+
         define_class_method "encrypt_#{attribute}" do |value|
           if options[:if] && !options[:unless]
             if value.nil? || (value.is_a?(String) && value.empty?)
@@ -162,7 +162,7 @@ module AttrEncrypted
             value
           end
         end
-        
+
         define_class_method "decrypt_#{attribute}" do |encrypted_value|
           if options[:if] && !options[:unless]
             if encrypted_value.nil? || (encrypted_value.is_a?(String) && encrypted_value.empty?)
@@ -177,7 +177,7 @@ module AttrEncrypted
             encrypted_value
           end
         end
-        
+
         define_method "#{attribute}" do
           begin
             value = instance_variable_get("@#{attribute}")
@@ -193,7 +193,7 @@ module AttrEncrypted
           end
           value
         end
-        
+
         define_method "#{attribute}=" do |value|
           begin
             original_options = [:key, :if, :unless].inject({}) do |hash, option|
@@ -210,7 +210,7 @@ module AttrEncrypted
       end
     end
     alias_method :attr_encryptor, :attr_encrypted
-    
+
     # Evaluates an option specified as a symbol representing an instance method or a proc
     #
     # If the option is not a symbol or proc then the original option is returned

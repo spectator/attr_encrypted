db5f642a22bc17dadaab996616d65f0824e6b094
Yury Velikanau <yury.velikanau@gmail.com>
2011-02-22 21:37:13 +0200

    Fix deprecation warning with AR3.

--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -21,12 +21,11 @@ create_people_table
 class Person < ActiveRecord::Base
   attr_encrypted :email, :key => 'a secret key'
   attr_encrypted :credentials, :key => Proc.new { |user| Encryptor.encrypt(:value => user.salt, :key => 'some private key') }, :marshal => true
-  
-  def after_initialize
+
+  def initialize(attributes = nil)
+    super(attributes)
     self.salt ||= Digest::SHA256.hexdigest((Time.now.to_i * rand(5)).to_s)
     self.credentials ||= { :username => 'example', :password => 'test' }
-  # Should be ActiveRecord::MissingAttributeError, however Rails 3 has this in ActiveModel,
-  # so that let's intecept everything until find better solution.
   rescue Exception => e
     raise unless ['ActiveRecord::MissingAttributeError', 'ActiveModel::MissingAttributeError'].include? e.class.to_s
   end
@@ -38,58 +37,58 @@ class PersonWithValidation < Person
 end
 
 class ActiveRecordTest < Test::Unit::TestCase
-  
+
   def setup
     ActiveRecord::Base.connection.tables.each { |table| ActiveRecord::Base.connection.drop_table(table) }
     create_people_table
   end
-  
+
   def test_should_encrypt_email
     @person = Person.create :email => 'test@example.com'
     assert_not_nil @person.encrypted_email
     assert_not_equal @person.email, @person.encrypted_email
     assert_equal @person.email, Person.find(:first).email
   end
-  
+
   def test_should_marshal_and_encrypt_credentials
     @person = Person.create
     assert_not_nil @person.encrypted_credentials
     assert_not_equal @person.credentials, @person.encrypted_credentials
     assert_equal @person.credentials, Person.find(:first).credentials
   end
-  
+
   def test_should_find_by_email
     @person = Person.create(:email => 'test@example.com')
     assert_equal @person, Person.find_by_email('test@example.com')
   end
-  
+
   def test_should_find_by_email_and_password
     Person.create(:email => 'test@example.com', :password => 'invalid')
     @person = Person.create(:email => 'test@example.com', :password => 'test')
     assert_equal @person, Person.find_by_email_and_password('test@example.com', 'test')
   end
-  
+
   def test_should_scope_by_email
     @person = Person.create(:email => 'test@example.com')
     assert_equal @person, Person.scoped_by_email('test@example.com').find(:first) rescue NoMethodError
   end
-  
+
   def test_should_scope_by_email_and_password
     Person.create(:email => 'test@example.com', :password => 'invalid')
     @person = Person.create(:email => 'test@example.com', :password => 'test')
     assert_equal @person, Person.scoped_by_email_and_password('test@example.com', 'test').find(:first) rescue NoMethodError
   end
-  
+
   def test_should_encode_by_default
     assert Person.attr_encrypted_options[:encode]
   end
-  
+
   def test_should_validate_presence_of_email
     @person = PersonWithValidation.new
     assert !@person.valid?
     assert ![nil, []].include?(@person.errors[:email])
   end
-  
+
   def test_should_validate_uniqueness_of_email
     @person = PersonWithValidation.new :email => 'test@example.com'
     assert @person.save
